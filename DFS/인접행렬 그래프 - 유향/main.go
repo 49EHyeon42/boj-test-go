c26256fa1404059db651c5a58dd0974eecc177d2
49EHyeon42 <49EHyeon42@gmail.com>
2021-12-04 15:28:37 +0900

    Add 인접행렬 그래프-유향

---
+++ DFS/인접행렬 그래프 - 유향/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+	graph  [][]bool
+)
+
+func main() {
+	var vertexCount, edgeCount int
+	fmt.Fscanln(reader, &vertexCount, &edgeCount)
+
+	graph = make([][]bool, vertexCount+1)
+	for i := range graph {
+		graph[i] = make([]bool, vertexCount+1)
+	}
+
+	var firstVertex, secondVertex int
+	for i := 0; i < edgeCount; i++ {
+		fmt.Fscan(reader, &firstVertex, &secondVertex)
+		graph[firstVertex][secondVertex] = true
+	}
+
+	fmt.Fprintln(writer, graph)
+
+	writer.Flush()
+}
+
+/*
+인접행렬-유향 그래프
+*/
