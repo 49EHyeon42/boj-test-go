4b85f7360ef3cfd6adecc29bd7aac75d4dcf342c
49EHyeon42 <49EHyeon42@gmail.com>
2021-12-05 13:47:51 +0900

    Rename 인접 행렬 그래프-무향-가중치

---
+++ DFS/인접 행렬 그래프-무향-가중치/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var vertexCount, edgeCount int
+	fmt.Scanln(&vertexCount, &edgeCount)
+
+	g := newGraph(vertexCount)
+
+	var firstVertex, secondVertex, weight int
+	for i := 0; i < edgeCount; i++ {
+		fmt.Scan(&firstVertex, &secondVertex, &weight)
+		g.AddEdge(firstVertex, secondVertex, weight)
+	}
+
+	fmt.Println(g)
+}
+
+func newGraph(vertexCount int) graph {
+	graph := make(graph, vertexCount)
+	for i := range graph {
+		graph[i] = make([]int, vertexCount)
+	}
+	return graph
+}
+
+type graph [][]int
+
+func (g *graph) AddEdge(firstVertex, secondVertex, weight int) {
+	// 가중치 관련 코드(거리, 중복 등)는 여기에 작성
+	(*g)[firstVertex-1][secondVertex-1] = weight
+	(*g)[secondVertex-1][firstVertex-1] = weight
+}
+
+// 인접 행렬 그래프-무향-가중치
